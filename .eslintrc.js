3eb5228b637984ceecdebf17660e35f1219204a4
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-01-02 18:22:48 +0800

    Use recommended Vue rules and turn off some

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,14 +7,19 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint"
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/essential",
-    "plugin:prettier/recommended"
-  ],
-  // required to lint *.vue files
-  plugins: ["vue"],
-  // add your custom rules here
-  rules: {}
+  extends: ["plugin:vue/recommended", "plugin:prettier/recommended"],
+  plugins: ["vue", "prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/component-name-in-template-casing": "off",
+    "vue/html-closing-bracket-newline": "off",
+    "vue/html-indent": "off",
+    "vue/html-self-closing": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/no-v-html": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/v-bind-style": "off"
+  }
 };
