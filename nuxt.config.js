337ad055724f9224196d2a781cc73e0578a68045
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-06-18 02:23:59 +0700

    Return deleted metas

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,9 @@ export default {
   head: {
     title: "Enlightenment News",
     meta: [
-      { hid: "description", name: "description", content: pkg.description },
-      {
-        "http-equiv": "Content-Security-Policy",
-        content: "upgrade-insecure-requests"
-      }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
