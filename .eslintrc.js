a8d066eb19187979e1d82237e061f8015de22d0d
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-06-18 00:46:19 +0700

    Update extends, plugins, and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,19 +7,13 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint"
   },
-  extends: ["plugin:vue/recommended", "plugin:prettier/recommended"],
-  plugins: ["vue", "prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/component-name-in-template-casing": "off",
-    "vue/html-closing-bracket-newline": "off",
-    "vue/html-indent": "off",
-    "vue/html-self-closing": "off",
-    "vue/max-attributes-per-line": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "vue/no-v-html": "off",
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/v-bind-style": "off"
-  }
+  extends: [
+    "@nuxtjs",
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/vue"
+  ],
+  plugins: ["prettier"],
+  rules: { "vue/v-bind-style": "off", "vue/no-v-html": "off" }
 };
