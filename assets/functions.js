6f7289712b9fe2d9c47fa18d5975e50b56546061
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-06-18 00:50:51 +0700

    Replace Forex with Fixer and use env

--- assets/functions.js
+++ assets/functions.js
@@ -1,9 +1,7 @@
 // API docs
-// https://1forge.com/forex-data-api/api-documentation#quotes
+// https://fixer.io/documentation
 // http://open-platform.theguardian.com/documentation/search
 // http://open-platform.theguardian.com/documentation/tag
-
-import { GUARDIAN_API_KEY, FOREX_API_KEY } from "~/assets/keys";
 
 function urlize(input) {
   return input
@@ -12,33 +10,33 @@ function urlize(input) {
     .replace(/[^-a-z0-9A-Z_]/g, "");
 }
 
-function getForexQuotesUrl(list) {
-  return `https://forex.1forge.com/1.0.3/quotes?pairs=${list}&api_key=${FOREX_API_KEY}`;
+function getLatestExchangeRateUrl(symbols) {
+  return `http://data.fixer.io/api/latest?access_key=${process.env.VUE_APP_FIXER_API_KEY}&${symbols}`;
 }
 
-function getHomeTopRatedContentUrl(section, size) {
-  return `https://content.guardianapis.com/search?star-rating=5&page-size=5&api-key=${GUARDIAN_API_KEY}`;
+function getHomeTopRatedContentUrl() {
+  return `https://content.guardianapis.com/search?star-rating=5&page-size=5&api-key=${process.env.VUE_APP_GUARDIAN_API_KEY}`;
 }
 
 function getHomeContentUrl(section, size) {
-  return `https://content.guardianapis.com/search?section=${section}&page-size=${size}&show-fields=trailText,thumbnail&show-tags=keyword&api-key=${GUARDIAN_API_KEY}`;
+  return `https://content.guardianapis.com/search?section=${section}&page-size=${size}&show-fields=trailText,thumbnail&show-tags=keyword&api-key=${process.env.VUE_APP_GUARDIAN_API_KEY}`;
 }
 
 function getSectionContentUrl(section) {
-  return `https://content.guardianapis.com/search?section=${section}&page-size=20&show-fields=trailText,thumbnail&show-tags=keyword&api-key=${GUARDIAN_API_KEY}`;
+  return `https://content.guardianapis.com/search?section=${section}&page-size=20&show-fields=trailText,thumbnail&show-tags=keyword&api-key=${process.env.VUE_APP_GUARDIAN_API_KEY}`;
 }
 
 function getSectionTagsUrl(section) {
-  return `https://content.guardianapis.com/tags?section=${section}&page-size=5&api-key=${GUARDIAN_API_KEY}`;
+  return `https://content.guardianapis.com/tags?section=${section}&page-size=5&api-key=${process.env.VUE_APP_GUARDIAN_API_KEY}`;
 }
 
 function getSearchResultsUrl(query) {
-  return `https://content.guardianapis.com/search?q=${query}&page-size=20&show-fields=trailText,thumbnail&show-tags=keyword&api-key=${GUARDIAN_API_KEY}`;
+  return `https://content.guardianapis.com/search?q=${query}&page-size=20&show-fields=trailText,thumbnail&show-tags=keyword&api-key=${process.env.VUE_APP_GUARDIAN_API_KEY}`;
 }
 
 export {
   urlize,
-  getForexQuotesUrl,
+  getLatestExchangeRateUrl,
   getHomeTopRatedContentUrl,
   getHomeContentUrl,
   getSectionContentUrl,
