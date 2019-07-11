bca621ac4187cc879586dfa1d2cd79721f5f917e
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-07-11 09:30:31 +0700

    Change nav bg

--- components/app-nav.vue
+++ components/app-nav.vue
@@ -42,7 +42,18 @@ export default {
 <style lang="postcss" scoped>
 nav {
   color: var(--white);
-  background-color: var(--black);
+  background: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0.15) 0%,
+      rgba(0, 0, 0, 0.15) 100%
+    ),
+    radial-gradient(
+        at top center,
+        rgba(255, 255, 255, 0.4) 0%,
+        rgba(0, 0, 0, 0.4) 120%
+      )
+      #989898;
+  background-blend-mode: multiply, multiply;
   box-shadow: var(--shadow);
 }
 
