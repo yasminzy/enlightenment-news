7fc36cfbe94903c3fa8e061089ed2dd649e67be1
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-04-24 00:05:47 +0800

    Update and rename file

---
+++ pages/[tag].vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <BaseTitle :title="title" />
+    <PostList :data="posts" :style="'stacked'" />
+  </div>
+</template>
+
+<script setup>
+import { useChangeCase } from "@vueuse/integrations/useChangeCase"
+import NProgress from "nprogress"
+
+const route = useRoute()
+const path = route.path.replace("/", "").replaceAll("%2F", "/")
+
+const tag = route.params.tag ? route.params.tag : path
+const key = useRuntimeConfig().public.guardianApiKey
+
+const { data } = await useFetch(
+  `https://content.guardianapis.com/search?tag=${tag}&total=18&page-size=18&show-fields=trailText,body,thumbnail&show-tags=keyword&api-key=${key}`
+).then()
+
+const title = computed(
+  () => `#${useChangeCase(getTitle(tag), "pascalCase").value}`
+)
+
+function getTitle(string) {
+  const parts = string.split("/")
+  return parts.pop()
+}
+
+const posts = computed(() => data.value?.response.results)
+
+if (data.value) {
+  NProgress.done()
+  window.scrollTo(0, 0)
+}
+</script>
