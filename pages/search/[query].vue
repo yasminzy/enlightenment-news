20790e3f8fdf2db232eeb2a67f55a03504c44635
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-04-24 00:04:37 +0800

    Move to a folder

---
+++ pages/search/[query].vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="max-w-[80ch]">
+    <SearchBox class="mb-8" />
+
+    <PostList :data="store.results" :style="'simple'" />
+  </article>
+</template>
+
+<script setup>
+import { useStore } from "@/stores/search"
+import NProgress from "nprogress"
+
+const store = useStore()
+const finished = computed(() => {
+  store.results.length
+})
+
+onMounted(() => {
+  if (finished) {
+    NProgress.done()
+    window.scrollTo(0, 0)
+  }
+})
+</script>
---
+++ pages/search/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <article class="max-w-[80ch]">
+    <SearchBox />
+  </article>
+</template>
+
+<script setup>
+import NProgress from "nprogress"
+
+onMounted(() => {
+  NProgress.done()
+  window.scrollTo(0, 0)
+})
+</script>
