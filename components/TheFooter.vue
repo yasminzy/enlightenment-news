067845fa13ad2b37fc4b1ed1043da7b90a37ea5a
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-04-23 23:52:32 +0800

    Edit footer and rename it

---
+++ components/TheFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="h-max mt-auto bg-stone-700 shadow-lg py-2 text-stone-50">
+    <div class="flex mx-auto max-w-7xl w-10/12 justify-between items-center">
+      <p>&copy; 2018-2022 Enlightenment News</p>
+
+      <a href="#header" class="text-stone-50">
+        <div class="text-5xl i-fe-arrow-up" />
+      </a>
+    </div>
+  </footer>
+</template>
