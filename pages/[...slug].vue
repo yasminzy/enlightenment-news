6ada9d11f945f52a24246d8060ec1c57434c246c
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2022-04-24 00:06:15 +0800

    Add catch all for the interpolation

---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+const route = useRoute()
+const path = route.path.replace("/", "").replaceAll("%2F", "/")
+
+await navigateTo({
+  name: "id",
+  params: { id: path }
+})
+</script>
